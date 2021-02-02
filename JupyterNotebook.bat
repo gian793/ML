3e95d7dd1d1598c596253401cbbe063fafac8d48
Marcolin <g.marcolin@osram.com>
2021-02-02 20:43:29 +0100

    First commit

---
+++ JupyterNotebook.bat
@@ -0,0 +1,1 @@
+start cmd.exe /k powershell.exe -ExecutionPolicy ByPass -NoExit -Command "& 'C:\ProgramData\Anaconda3\shell\condabin\conda-hook.ps1' ; conda activate 'C:\ProgramData\Anaconda3' " ; jupyter notebook --notebook-dir=C:\MyFolder\Local_Docs\Trainings\MachineLearning\MLGithub
